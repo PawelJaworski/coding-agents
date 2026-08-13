bd83b145d3901a9e3247f8d41c7c00c1f0d190b2
Pawel Jaworski <pawel.jaworski-extern@dbschenker.com>
2026-08-13 22:27:28 +0200

    event stream

--- skills/backend-development/templates/SomethingHappenToFooEvent.java
+++ skills/backend-development/templates/SomethingHappenToFooEvent.java
@@ -3,8 +3,8 @@ package {base}.domain.events;
 import java.util.UUID;
 
 //Class name ends with Event
-//Always contains aggregate id. If aggregate is Foo then id attribute is named fooId.
-public record SomethingHappenToFooEvent(UUID fooId, /**other attributes**/) {
+//Always contains aggregate id
+public record SomethingHappenToFooEvent(UUID aggregateId, /**other attributes**/) {
     @Override
     public DomainEventType eventType() {
         return DomainEventType.SOMETHING_HAPPEN_TO_FOO;

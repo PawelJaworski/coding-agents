b120e91acb541d598d505b5e9a9346bc4e6d02b3
Pawel Jaworski <pawel.jaworski-extern@dbschenker.com>
2026-08-14 13:38:28 +0200

    small fix

--- skills/backend-development/templates/DoSomethingOnFooHandler.java
+++ skills/backend-development/templates/DoSomethingOnFooHandler.java
@@ -3,12 +3,13 @@ package {base}.dosomethingonfoo;
 import {base}.eventstream.CommandHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.UUID;
 
 @Component
 @Transactional
 public class DoSomethingOnFooHandler implements CommandHandler<DoSomethingOnFooCmd> {
     @Override
-    public Long handle(DoSomethingOnFooCmd command) {
+    public UUID handle(DoSomethingOnFooCmd command) {
         return null;
     }
 }
